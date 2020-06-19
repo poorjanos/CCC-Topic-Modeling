a201c92b10bcf6f7e25744c5a4d78d2afacfcd85
Janos Poor <poorjanos@gmail.com>
2020-06-19 15:55:49 +0200

    Add LDA

--- src/processing/tokenizer.py
+++ src/processing/tokenizer.py
@@ -35,6 +35,26 @@ def tokenize_1gram(text, model, ents = []):
     return res
 
 
+def remove_single(df, tokens_col):
+    '''
+    Removes words that only appear once in corpus 
+    
+    Parameters:
+    ----------
+    pd_series : pandas Series (df col)
+    
+    Returns:
+    ----------
+    Cleaned docs as list of lists
+
+    '''
+    all_tokens = sum(df[tokens_col], [])
+    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    tokenized_no_single = [[word for word in text if word not in tokens_once] for text in df[tokens_col]]
+
+    return tokenized_no_single
+    
+    
 if __name__ == '__main__':
     # Load hu model
     nlp = hu.load()
@@ -46,6 +66,8 @@ if __name__ == '__main__':
     # Run tokenizer
     df['tokenized'] = df['text'].apply(lambda x: tokenize_1gram(clean(x), model = nlp, ents = ['PER', 'LOC']))
     df['token_count'] = df['tokenized'].apply(lambda x: len(x))
+    df['tokenized_no_single'] = remove_single(df, 'tokenized')
+    df['tokenized_no_single_count'] = df['tokenized_no_single'].apply(lambda x: len(x))
     df.to_csv(path_data / 'tokenized1gram_data.csv', index = False)
 
 
