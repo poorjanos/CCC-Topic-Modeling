3331e8b01a768503518c2e45ba58795ab476ee57
Janos Poor <poorjanos@gmail.com>
2020-07-10 14:45:49 +0200

    Add topic pattern extraction

--- src/models/build_topic_model.py
+++ src/models/build_topic_model.py
@@ -1,5 +1,6 @@
 # Helper functions for topic modeling
 import numpy as np
+import pandas as pd
 import gensim.corpora as corpora
 from gensim import models
 from gensim.models import CoherenceModel
@@ -56,3 +57,46 @@ def drop_topics(w, h, drop_list):
     '''
     keeps = [i for i in range(w.shape[1]) if i not in drop_list]
     return w[:, keeps], h[keeps, :]
+
+
+def sparse_argsort(arr):
+    '''
+    Argsort for 1D array ignoring zeros
+    '''
+    indices = np.nonzero(arr)[0]
+    return indices[np.argsort(arr[indices])]
+
+def get_topic_patterns(dtm, topic_names, threshold=0):
+    '''
+    Extract topic patterns from doc-topic matrix returned by NMF
+    Filters  non-relevant topics by threshold
+    
+    Parameters:
+    ----------
+    dtm : doc-topic matrix from NMF
+    threshold: min weight for topic to be picked up
+    lut: look-up table for topic names
+    
+    Returns:
+    -------
+    Pandas df with topic patterns and frequency metrics
+    '''
+    
+    # Set limit for topic weigth and zero out topics below
+    mask = dtm >= threshold
+    masked_dtm = np.where(mask, dtm, 0)
+    
+    # Extract topic patterns
+    topic_patterns = [sparse_argsort(i) for i in masked_dtm]
+    topic_patterns = [tuple(np.array(topic_names)[i]) for i in topic_patterns]
+    
+    # Add to df and compute frequency metrics
+    topic_patterns_df = pd.DataFrame()
+    topic_patterns_df['pattern'] = topic_patterns
+    
+    topic_freq = topic_patterns_df.groupby(['pattern']).size().reset_index(name = 'count')\
+            .sort_values(by=['count'], ascending = False).reset_index(drop = True)
+    topic_freq['pct'] = topic_freq['count']/sum(topic_freq['count'])
+    topic_freq['pct roll'] = topic_freq['count'].cumsum()/sum(topic_freq['count'])
+    
+    return topic_freq
